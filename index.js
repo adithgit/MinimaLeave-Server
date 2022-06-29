0fe52b1a6ee61822a741926e82cd18f0433c40d7
adithgit <5285@mbcpeermade.com>
2022-06-29 23:31:59 +0530

    initial files

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+require('./db');
+
+app.post('/signup', (req, res)=>{
+    
+    if( role === 'admin'){
+        // check if admin exists.
+        // create admin.
+    }
+    else if( role === 'parent'){
+        // check if parent exists
+        // Check if their children exists
+        // create parent
+    }
+    else if( role === 'student'){
+        // check if they already exists
+        // create student
+    }
+})
+
+app.post('/login',(req,res)=>{
+    if( user === 'admin'){
+        // check if admin exists 
+    }
+    else if ( user === 'student'){
+        // Check if student exists 
+    }
+    else if( user === 'parent'){
+        // check if parent exists
+    }
+})
+
+app.get('/apply', (req,res)=>{
+    // check if student is logged in
+    // logic for granting leave
+})
+
+app.get('/grant', (req, res)=>{
+    if( user === 'admin' ){
+        // check if logged in
+        // check if parent granted permission 
+        // grant or decline permission
+    }
+    if( user === 'parent'){
+        // check if logged in 
+        // grant permission -> have to wait for admin to authorize
+    }
+})
+
+app.listen(8080,()=>{
+    console.log('listening to port 8080');
+})
